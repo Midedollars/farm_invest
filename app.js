095c7c8418ad16b393b956d7ce3353664cb85ad7
Midedollars <olanrewajuolamide100@gmail.com>
2022-04-11 21:15:07 +0800

    new commit

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const { WELCOME, v1 } = constants
 const apiRoutes = require('./routes/v1')
 const config = require('./config/config')
 const db = require('./database/postgresql')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 
 const {
@@ -18,7 +25,15 @@ app.use(json())
 // adds middleware that parses requests with x-www-form-urlencoded data encoding
 app.use(urlencoded({ extended: true }))
 // adds a heartbeat route for the culture
-app.get('/', (req, res) => successResponse(res, { message: WELCOME }))
+// create home route
+app.get('/auth', (req, res) => {
+  res.render('home', { user: req.user });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+// app.get('/', (req, res) => successResponse(res, { message: WELCOME }))
 // serves v1 api routes
 app.use(v1, apiRoutes)
 // catches 404 errors and forwards them to error handlers
@@ -28,11 +43,41 @@ app.use((req, res, next) => {
 //handles all forwarded errors
 app.use((err, req, res, next) => errorResponse(req, res, err))
 
+// set view engine
+app.set('view engine', 'ejs');
 
+// set up session cookies
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.COOKIEKEY]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set up routes
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+
+
+const dotenv = require("dotenv");
+dotenv.config();
+const { DATABASE_URI } = process.env;
 
 // initialize the port constant
 const port = config.PORT || 3000
 // server listens for connections
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await mongoose.connect('mongodb+srv://space-ex:12345@cluster0.kh4cu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database is connected");
+  } catch (error) {
+    console.log(`Database Is Not Connected`);
+  }
   console.log('Server started listening on port', port)
 })
--- config/keys.js
+++ config/keys.js
@@ -9,6 +9,6 @@ module.exports = {
         },
 
     session: {
-        cookieKey: process.env.COOKIEKEY,
+        COOKIEKEY: process.env.COOKIEKEY,
       },
 };
